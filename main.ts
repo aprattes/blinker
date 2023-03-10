7e49f2b2229dff907004d6d7ae8dee54b9d12c30
Alexander Prattes <8425495+aprattes@users.noreply.github.com>
2023-03-10 13:39:00 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,26 @@
+input.onButtonPressed(Button.A, function () {
+    zustand = links
+})
+input.onButtonPressed(Button.B, function () {
+    zustand = rechts
+})
+let rechts = 0
+let zustand = 0
+let links = 0
+links = 2
+zustand = 0
+rechts = 1
+basic.forever(function () {
+    if (zustand == rechts) {
+        basic.showArrow(ArrowNames.East)
+    } else {
+        if (zustand == links) {
+            basic.showArrow(ArrowNames.West)
+        } else {
+        	
+        }
+    }
+    basic.pause(500)
+    basic.clearScreen()
+    basic.pause(500)
+})
